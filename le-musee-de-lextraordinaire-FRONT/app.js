3832e582d3b7f7b9fda33c7cd0e2eda5621846f5
amelie-chardon <amelie.chardon@laplateforme.io>
2021-01-12 12:55:24 +0100

    ajout bouton AppButton + polices

--- le-musee-de-lextraordinaire-FRONT/app.js
+++ le-musee-de-lextraordinaire-FRONT/app.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppButton from './components/AppButton';
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Pouet</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component {
+
+    //Chargement des polices
+    componentDidMount = async () => {
+        await Font.loadAsync({
+            'BodoniModa': require('./assets/fonts/BodoniModa_6pt-Regular.ttf'),
+            'LinuxLibertine': require('./assets/fonts/linlibertine_dr-webfont.ttf'),
+        });
+        this.setState({ loading: false })
+    }
+
+    render() {
+
+        return (
+            <View style={styles.container}>
+                <Text>Hello world !!!</Text>
+                <StatusBar style="auto" />
+                <AppButton
+                    //onPress={onPress}
+                    title="Connexion"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+            </View>
+
+        )
+    }
 }
 
 const styles = StyleSheet.create({
